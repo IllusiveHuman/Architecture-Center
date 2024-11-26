a73ffc53fd05573e1b49006165e7207688cd7250
IllusiveHuman <149196317+IllusiveHuman@users.noreply.github.com>
2024-11-26 14:21:18 +0200

    Add files via upload
    Adaptive only for >=1600px and 320px-480px

---
+++ Architecture Center/script.js
@@ -0,0 +1,184 @@
+
+const slideValue_1 = document.querySelector('.current-slider-value-1');
+const slideValue_2 = document.querySelector('.current-slider-value-2');
+const inputSlider_1 = document.querySelector('.input-range-floor');
+const inputSlider_2 = document.querySelector('.input-range-time');
+const parametrs_1 = document.querySelectorAll('.value-1');
+const parametrs_2 = document.querySelectorAll('.value-2');
+
+const navigation = document.querySelector('.navigation');
+const btn_menu = document.querySelector('.menu');
+
+if (window.innerWidth > 480) {
+    btn_menu.style.display = "none"
+}
+
+
+btn_menu.addEventListener('click', () => {
+    navigation.classList.toggle('show-mobile-navigation');
+    console.log(1)
+})
+
+// Поднятие крайних значений ползунка
+
+const initSlider = function () {
+
+    inputSlider_1.oninput = (() => {
+
+
+        let value = inputSlider_1.value;
+        slideValue_1.textContent = value;
+
+        let min = inputSlider_1.min;
+        let max = inputSlider_1.max;
+        let percent = ((value - min) / (max - min)) * 100;
+
+        let progress = (value - min) / (max - min);
+
+        let translateX = 50 - progress * 100;
+
+        if (value == 10 || value == 160) {
+            slideValue_1.style.display = 'none'
+        }
+        else {
+            slideValue_1.style.display = 'block'
+
+        }
+
+        slideValue_1.style.left = `calc(${percent}% - ${slideValue_1.offsetWidth / 2}px)`;;
+        console.log(slideValue_1.offsetWidth)
+        slideValue_1.style.transform = `translateX(${translateX}%)`;
+
+        if ((value >= 11 && value <= 17) || (value >= 147 && value <= 159)) {
+            Array.from(parametrs_1).forEach((el) => {
+                el.classList.add("value-translate")
+            })
+        }
+        else {
+            Array.from(parametrs_1).forEach((el) => {
+                el.classList.remove("value-translate")
+            })
+        }
+    })
+
+    inputSlider_2.oninput = (() => {
+
+
+        let value = inputSlider_2.value;
+        slideValue_2.textContent = value;
+
+        let min = inputSlider_2.min;
+        let max = inputSlider_2.max;
+        let percent = ((value - min) / (max - min)) * 100;
+
+        let progress = (value - min) / (max - min);
+
+        let translateX = 50 - progress * 100;
+
+        if (value == 4 || value == 60) {
+            slideValue_2.style.display = 'none'
+        }
+        else {
+            slideValue_2.style.display = 'block'
+
+        }
+
+        slideValue_2.style.left = `calc(${percent}% - ${slideValue_2.offsetWidth / 2}px)`;;
+        slideValue_2.style.transform = `translateX(${translateX}%)`;
+
+        if ((value >= 5 && value <= 6) || (value >= 57 && value <= 59)) {
+            Array.from(parametrs_2).forEach((el) => {
+                el.classList.add("value-translate")
+            })
+        }
+        else {
+            Array.from(parametrs_2).forEach((el) => {
+                el.classList.remove("value-translate")
+            })
+        }
+    })
+}
+
+initSlider()
+
+/*Автоширина Select/range/calculateBTN */
+
+const allSelectorsBlocks = document.querySelectorAll('.select-wrapper')
+const allInsideSelectorsEl = document.querySelectorAll('.select-wrapper select')
+const range_1 = document.querySelector('.range-1')
+const range_2 = document.querySelector('.range-2')
+const input_range_time = document.querySelector('.input-range-time')
+const input_range_floor = document.querySelector('.input-range-floor')
+const calculate_cost = document.querySelector('.calculate-cost')
+
+console.log()
+
+const initAllSelectors = function () {
+    allSelectorsBlocks.forEach(element => {
+        element.style.width = `${window.innerWidth * 0.85}px`
+    })
+
+    allInsideSelectorsEl.forEach(element => {
+        element.style.width = `${window.innerWidth * 0.85}px`
+    })
+    range_1.style.width = `${window.innerWidth * 0.85}px`
+    range_2.style.width = `${window.innerWidth * 0.85}px`
+    input_range_time.style.width = `${window.innerWidth * 0.85}px`
+    input_range_floor.style.width = `${window.innerWidth * 0.85}px`
+    calculate_cost.style.width = `${window.innerWidth * 0.85}px`
+}
+
+if (window.innerWidth <= 480) { initAllSelectors() }
+
+
+/*Выравния кнопки расчёта и сброса фильтров */
+
+const clearFilters = document.querySelector('.clear-filters');
+
+const initClearFilterBtn = function () {
+    const margin_left = (window.innerWidth - parseFloat(calculate_cost.style.width)) / 2;
+    console.log(margin_left)
+    clearFilters.style.marginLeft = `${margin_left}px`
+}
+
+if (window.innerWidth <= 480) { initClearFilterBtn() }
+
+
+const calculate_buttons = document.querySelector('.calculate-buttons')
+const autoMarginCalculateBtn = function () {
+    const selectors = document.querySelector('.selectors').getBoundingClientRect()
+    const selector_element = document.querySelector('.select-wrapper').getBoundingClientRect()
+
+
+    calculate_buttons.style.marginLeft = `${selector_element.left - selectors.left}px`
+}
+
+
+if (window.innerWidth > 480) autoMarginCalculateBtn()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
